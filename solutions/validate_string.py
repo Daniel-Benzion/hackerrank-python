ec5d95d802fa2cae26affd43b516d7b74f68406f
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-08-13 14:01:22 -0500

    validate_string

---
+++ solutions/validate_string.py
@@ -0,0 +1,6 @@
+def validate_string(s):
+    print(any(char.isalnum() for char in s))
+    print(any(char.isalpha() for char in s))
+    print(any(char.isdigit() for char in s))
+    print(any(char.islower() for char in s))
+    print(any(char.isupper() for char in s))
