ee69314027e47c06aec979fb369003f8bcd58602
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-09-02 17:04:58 -0500

    exceptions

---
+++ solutions/exceptions.py
@@ -0,0 +1,7 @@
+def exceptions():
+    for i in range(int(input())):  
+        try:
+            a, b= map(int, input().split())
+            print(a // b)
+        except BaseException as e:
+            print("Error Code:", e)
